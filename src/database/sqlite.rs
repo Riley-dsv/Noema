a561caed489caeb4840e37d0cbaab0f9e616dc3c
Riley-dsv <riley.dsv@gmail.com>
2026-06-09 06:10:44 +0200

    refactor(database): Make the database API stable
    A lot of change appears, first the use of a store to avoid repetition
    reduce responsibilites of the database, use borrowing everywhere.
    Also changed the ID format for a 24 bits UUID (8 hex char)

--- src/database/sqlite.rs
+++ src/database/sqlite.rs
@@ -1,21 +1,8 @@
 use std::path::PathBuf;
 
 use chrono;
-use rusqlite::{Connection, Result, params};
-
-use crate::database::path;
-
-fn connect(db_path: Option<PathBuf>) -> Result<Connection> {
-    let path = db_path
-        .map(PathBuf::from)
-        .unwrap_or_else(path::default_database_path);
-
-    if let Some(parent) = path.parent() {
-        std::fs::create_dir_all(parent).expect("failed to create database directory");
-    }
-
-    Connection::open(path)
-}
+use rusqlite::{Connection, Result, Row, params};
+use uuid::Uuid;
 
 #[derive(Debug)]
 pub struct Note {
@@ -26,126 +13,130 @@ pub struct Note {
     pub updated_at: String,
 }
 
-pub fn init(db_path: Option<PathBuf>) -> Result<()> {
-    let conn = connect(db_path)?;
-
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS notes (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT NOT NULL,
-      content TEXT,
-      created_at TEXT,
-      updated_at TEXT
-    )",
-        (),
-    )?;
-
-    Ok(())
+pub struct SQLStore {
+    connection: Connection,
 }
 
-pub fn insert(db_path: Option<PathBuf>, note_title: String, note_content: String) -> Result<()> {
-    let conn = connect(db_path)?;
-    let now = chrono::offset::Local::now().to_rfc3339();
-
-    conn.execute(
-        "INSERT INTO notes (title, content, created_at, updated_at) VALUES (?1, ?2, ?3, ?4)",
-        params![note_title, note_content, now, now],
-    )?;
-
-    Ok(())
-}
-
-pub fn list(db_path: Option<PathBuf>) -> Result<()> {
-    let conn = connect(db_path)?;
-    let mut statement = conn.prepare(
-        "SELECT id, title, content, created_at, updated_at FROM notes ORDER BY updated_at DESC",
-    )?;
-
-    let note_iter = statement.query_map([], |row| {
-        Ok(Note {
-            id: row.get(0)?,
-            title: row.get(1)?,
-            content: row.get(2)?,
-            created_at: row.get(3)?,
-            updated_at: row.get(4)?,
-        })
-    })?;
-
-    for note_result in note_iter {
-        let note = note_result?;
-        println!(
-            "Found note: {} | {} | {}",
-            note.id, note.title, note.updated_at
-        )
+impl SQLStore {
+    pub fn open(db_path: PathBuf) -> Result<Self> {
+        let connection = connect(db_path)?;
+        Ok(Self { connection })
     }
 
-    Ok(())
-}
+    pub fn init(&self) -> Result<()> {
+        self.connection.execute(
+            "CREATE TABLE IF NOT EXISTS notes (
+              id TEXT PRIMARY KEY,
+              title TEXT NOT NULL,
+              content TEXT,
+              created_at TEXT,
+              updated_at TEXT
+            )",
+            (),
+        )?;
 
-pub fn delete(db_path: Option<PathBuf>, id: String) -> Result<()> {
-    let conn = connect(db_path)?;
-
-    let deleted = conn.execute("DELETE FROM notes WHERE id=?1", params![id])?;
-
-    if deleted == 0 {
-        print!("No note found with id {id}");
+        Ok(())
     }
 
-    Ok(())
+    pub fn insert_note(&self, note_title: &str, note_content: &str) -> Result<()> {
+        let id = new_note_id();
+        let now = chrono::offset::Local::now().to_rfc3339();
+
+        self.connection.execute(
+            "INSERT INTO notes (id, title, content, created_at, updated_at) VALUES (?1, ?2, ?3, ?4)",
+            params![id, note_title, note_content, now, now],
+        )?;
+
+        Ok(())
+    }
+
+    pub fn list_notes(&self) -> Result<Vec<Note>> {
+        let mut statement = self.connection.prepare(
+            "SELECT id, title, content, created_at, updated_at FROM notes ORDER BY updated_at DESC",
+        )?;
+
+        Ok(statement
+            .query_map([], note_from_row)?
+            .collect::<Result<Vec<_>>>()?)
+    }
+
+    pub fn delete_note(&self, id: &str) -> Result<usize> {
+        let deleted = self
+            .connection
+            .execute("DELETE FROM notes WHERE id=?1", params![id])?;
+
+        if deleted == 0 {
+            print!("No note found with id {id}");
+        }
+
+        Ok(deleted)
+    }
+
+    pub fn get_content(&self, id: &str) -> Result<String> {
+        let note = self.connection.query_row(
+            "SELECT content FROM notes WHERE id=?1",
+            params![id],
+            note_from_row,
+        );
+
+        Ok(note.unwrap().content)
+    }
+
+    pub fn get_note(&self, id: &str) -> Result<Note> {
+        let note = self.connection.query_row(
+            "SELECT id, title, content, created_at, updated_at FROM notes WHERE id=?1",
+            params![id],
+            note_from_row,
+        )?;
+
+        Ok(note)
+    }
+    pub fn update_content(&self, id: &str, new_content: &str) -> Result<()> {
+        let now = chrono::offset::Local::now().to_rfc3339();
+
+        self.connection.execute(
+            "UPDATE notes SET content=?1, updated_at=?2 WHERE id=?3",
+            params![new_content, now, id],
+        )?;
+
+        Ok(())
+    }
+
+    pub fn update_title(&self, id: &str, title: &str) -> Result<()> {
+        let now = chrono::offset::Local::now().to_rfc3339();
+
+        self.connection.execute(
+            "UPDATE notes SET title=?1, updated_at=?2 WHERE id=?3",
+            params![title, now, id],
+        )?;
+
+        Ok(())
+    }
 }
 
-pub fn get_content(db_path: Option<PathBuf>, id: &str) -> Result<String> {
-    let conn = connect(db_path)?;
-    let content = conn.query_row(
-        "SELECT content FROM notes WHERE id=?1",
-        params![id],
-        |row| row.get(0),
-    );
+fn connect(path: PathBuf) -> Result<Connection> {
+    if let Some(parent) = path.parent() {
+        std::fs::create_dir_all(parent).expect("failed to create database directory");
+    }
 
-    Ok(content?)
+    Connection::open(path)
 }
 
-pub fn select(db_path: Option<PathBuf>, id: String) -> Result<Note> {
-    let conn = connect(db_path)?;
-    let note = conn.query_row(
-        "SELECT id, title, content, created_at, updated_at FROM notes WHERE id=?1",
-        params![id],
-        |row| {
-            Ok(Note {
-                id: row.get(0)?,
-                title: row.get(1)?,
-                content: row.get(2)?,
-                created_at: row.get(3)?,
-                updated_at: row.get(4)?,
-            })
-        },
-    )?;
-
-    Ok(note)
+fn note_from_row(row: &Row) -> Result<Note> {
+    Ok(Note {
+        id: row.get("id")?,
+        title: row.get("title")?,
+        content: row.get("content")?,
+        created_at: row.get("created_at")?,
+        updated_at: row.get("updated_at")?,
+    })
 }
 
-pub fn update(db_path: Option<PathBuf>, new_content: &str, id: String) -> Result<()> {
-    let conn = connect(db_path)?;
-    let now = chrono::offset::Local::now().to_rfc3339();
-
-    println!("{id}");
-
-    conn.execute(
-        "UPDATE notes SET content=?1, updated_at=?2 WHERE id=?3",
-        params![new_content, now, id],
-    )?;
-
-    Ok(())
-}
-
-pub fn update_title(db_path: Option<PathBuf>, id: &str, title: String) -> Result<()> {
-    let conn = connect(db_path)?;
-    let now = chrono::offset::Local::now().to_rfc3339();
-
-    conn.execute(
-        "UPDATE notes SET title=?1, updated_at=?2 WHERE id=?3",
-        params![title, now, id],
-    )?;
-
-    Ok(())
+fn new_note_id() -> String {
+    Uuid::new_v4()
+        .simple()
+        .to_string()
+        .chars()
+        .take(8)
+        .collect()
 }
